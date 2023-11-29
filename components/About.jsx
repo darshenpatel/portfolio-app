d1576541a7fa044c84c79dd6990af7dd4ae0cf3d
Darshen <darshen.patel10@gmail.com>
2023-11-29 13:08:20 -0500

    Reinstalling the nextjs.yml file

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     <div id='about' className='w-full p-2 flex items-center py-3'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-          <h3 class='pb-4'>About</h3>
+          <h3 className='pb-4'>About</h3>
           <p>I am an adept Senior Product Manager with a unique background that seamlessly bridges healthcare aspirations and technological innovation. Having transitioned from the pursuit of medical school to embracing the dynamism of tech and entrepreneurship, I possess a rare blend of experiences. My expertise is further enriched by substantial involvement in supply chain management across two different startups. Now, I am poised to channel my skills and passion into a mission-driven role within the healthcare sector, eager to contribute to a company that aligns with my commitment to making a difference in people's lives through technology.</p>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-5 hover:scale-105 ease-in duration-300'>
@@ -22,7 +22,7 @@ const About = () => {
             href='/Darshen-Patel-Resume.pdf'
             download='Darshen-Patel-Resume.pdf'
           >
-            <div class='flex items-center border-2 border-black p-2 rounded-md cursor-pointer hover:bg-gray-100 -mt-12'>
+            <div className='flex items-center border-2 border-black p-2 rounded-md cursor-pointer hover:bg-gray-100 -mt-12'>
               <img width="30" height="30" src="https://img.icons8.com/ios/50/resume.png" alt="resume"/>
               <p className='ml-5'>Darshen Patel's Resume</p>
             </div>
