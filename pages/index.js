152922d5a7077f5000e81856a00869a977e117cc
Darshen <darshen.patel10@gmail.com>
2023-11-29 12:03:49 -0500

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import Main from '../components/Main';
 import About from '../components/About';
-import Projects from '../components/Projects';
-import Skills from '../components/Skills';
-// import Interests from '../components/Interests';
 
 
 export default function Home() {
@@ -19,9 +16,6 @@ export default function Home() {
             </Head>
             <Main />
             <About />
-            {/* <Projects />
-            <Skills /> */}
-            {/* <Interests /> */}
         </div>
     )
 }
