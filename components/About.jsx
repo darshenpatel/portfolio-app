821503451ea3672123223f8fa75ba89f0d230440
Darshen <darshen.patel10@gmail.com>
2023-12-03 14:07:56 -0500

    Updates to the About component.

--- components/About.jsx
+++ components/About.jsx
@@ -11,7 +11,9 @@ const About = () => {
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
           <h3 className='pb-4'>About</h3>
-          <p>I am an adept Senior Product Manager with a unique background that seamlessly bridges healthcare aspirations and technological innovation. Having transitioned from the pursuit of medical school to embracing the dynamism of tech and entrepreneurship, I possess a rare blend of experiences. My expertise is further enriched by substantial involvement in supply chain management across two different startups. Now, I am poised to channel my skills and passion into a mission-driven role within the healthcare sector, eager to contribute to a company that aligns with my commitment to making a difference in people's lives through technology.</p>
+          <p>
+            As an experienced Senior Product Manager, I have a diverse background that uniquely positions me at the intersection of healthcare, technology, and entrepreneurship. Initially aspiring towards a career in medicine, I shifted focus to harness the dynamic potential of technology and innovation. This transition has endowed me with a rare combination of insights and skills. My journey in the tech world has been marked by significant involvement in supply chain management, where I have played pivotal roles in two startups. This experience has not only honed my abilities in streamlining operation workflows but also given me a profound understanding of how to criticality and efficiently, handle the technical foundations for scale. My commitment lies in leveraging technology to make a tangible difference in people's lives. I am particularly interested in opportunities that align with my dedication to healthcare innovation, sustainable supply chain management, environmental stewardship, and educational advancement.
+          </p>
         </div>
         <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-5 hover:scale-105 ease-in duration-300'>
           <Image className='rounded-md' src={AboutImg} alt=''/>
@@ -22,7 +24,7 @@ const About = () => {
             href='/Darshen-Patel-Resume.pdf'
             download='Darshen-Patel-Resume.pdf'
           >
-            <div className='flex items-center border-2 border-black p-2 rounded-md cursor-pointer hover:bg-gray-100 -mt-12'>
+            <div className='flex items-center border-2 border-black p-2 rounded-md cursor-pointer hover:bg-gray-100 -mt-1'>
               <img width="30" height="30" src="https://img.icons8.com/ios/50/resume.png" alt="resume"/>
               <p className='ml-5'>Darshen Patel's Resume</p>
             </div>
